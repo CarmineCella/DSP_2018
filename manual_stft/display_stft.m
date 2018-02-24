aabd3c39de35d6a1a54a6ffec4911a63890fc41f
CarmineCella <carmine.emanuele.cella@gmail.com>
2018-02-24 12:38:32 +0100

    added log amplitude

--- manual_stft/display_stft.m
+++ manual_stft/display_stft.m
@@ -2,5 +2,5 @@ function display_stft (filename)
     spect = textread (filename); %#ok<DTXTRD>
     sz = size(spect, 2);
     spect = spect(:,floor( sz/2:sz));
-    imagesc (spect');
+    imagesc (log(spect'));
 end
